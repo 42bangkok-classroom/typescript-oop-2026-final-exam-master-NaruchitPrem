dd5ba36247613374af097d0cef68c4e7c489f0c8
Naruchit Choenghom <68010578@kmitl.ac.th>
2026-03-25 10:35:01 +0700

    quiz1

--- src/products/product.controller.ts
+++ src/products/product.controller.ts
@@ -5,7 +5,7 @@ import { ProductService } from './product.service';
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
-  @Get()
+  @Get('products')
   findAll() {
     const result = this.productService.findAll();
     return {
