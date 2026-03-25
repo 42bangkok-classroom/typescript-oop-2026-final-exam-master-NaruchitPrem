2ffddbc9316dec6a5180510bef444adb65eddd72
Naruchit Choenghom <68010578@kmitl.ac.th>
2026-03-25 10:31:19 +0700

    quiz1

---
+++ src/products/product.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get } from '@nestjs/common';
+import { ProductService } from './product.service';
+
+@Controller('products')
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+
+  @Get()
+  findAll() {
+    const result = this.productService.findAll();
+    return {
+      success: true,
+      data: result,
+      message: 'Fetched products successfully',
+    };
+  }
+}
---
+++ src/products/product.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import * as fs from 'fs';
+import * as path from 'path';
+import { Product } from './product.interface';
+
+@Injectable()
+export class ProductService {
+  private readonly dataPath = path.join(process.cwd(), 'data', 'products.json');
+
+  test() {
+    return [];
+  }
+
+  findAll(): Product[] {
+    const rawData = fs.readFileSync(this.dataPath, 'utf-8');
+    const users = JSON.parse(rawData) as Product[];
+    return users;
+  }
+}
